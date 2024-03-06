05967e752ecbfb1b7b0e3a07b6a68c0bc253bcd7
Maarten van den Hooven <maartenv@kontent.ai>
2024-03-06 14:19:34 +0100

    move smart link in hero unit

--- components/shared/HeroUnit.tsx
+++ components/shared/HeroUnit.tsx
@@ -59,10 +59,10 @@ export const HeroUnitComponent: FC<Props> = (props) => {
       url={props.item.elements.backgroundImage.value[0]?.url || ''}
       itemId={props.item.system.id}
       type={props.item.elements.backgroundImage.value[0]?.type}
+      {...createItemSmartLink(props.item.system.id, props.item.system.name)}
     >
       <div
         className={`py-5 md:py-5 px-3 w-full flex justify-center`}
-        {...createItemSmartLink(props.item.system.id, props.item.system.name)}
       >
         <div
           className={`${mainColorTextClass[siteCodename]} m-0 text-3xl tracking-wide font-semibold text-center`}
