8a4e88ec2519630c42b538e962ea79a66cb4504e
Maarten van den Hooven <maartenv@kontent.ai>
2024-03-06 14:29:02 +0100

    hero unit smart link

--- components/shared/HeroUnit.tsx
+++ components/shared/HeroUnit.tsx
@@ -26,10 +26,11 @@ export const HeroUnitComponent: FC<Props> = (props) => {
       props.item.elements.content.value.length === 0
     ) {
       return (
-        <div className='comp_hero-unit py-1 px-3 w-full flex justify-center flex-col items-center'
-          {...createElementSmartLink(
-            contentTypes.hero_unit.elements.content.codename
-          )}>
+        <div className='comp_hero-unit py-1 px-3 w-full flex justify-center flex-col items-center'        
+        {...createItemSmartLink(props.item.system.id, props.item.system.name)}
+        {...createElementSmartLink(
+          contentTypes.hero_unit.elements.content.codename
+        )}>
           <div
             className='hidden md:block m-0 text-xl font-medium break-words hyphens-none text-white text-center w-full lg:w-1/2 pb-2 max-w-5xl'
           >
@@ -51,32 +52,30 @@ export const HeroUnitComponent: FC<Props> = (props) => {
   }
 
   return (
-    <div
-      {...createItemSmartLink(props.item.system.id, props.item.system.name)}>
-      <HeroImage
-        alt={
-          props.item.elements.backgroundImage.value[0]?.description ||
-          'Hero image'
-        }
-        url={props.item.elements.backgroundImage.value[0]?.url || ''}
-        itemId={props.item.system.id}
-        type={props.item.elements.backgroundImage.value[0]?.type}
+    <HeroImage
+      alt={
+        props.item.elements.backgroundImage.value[0]?.description ||
+        'Hero image'
+      }
+      url={props.item.elements.backgroundImage.value[0]?.url || ''}
+      itemId={props.item.system.id}
+      type={props.item.elements.backgroundImage.value[0]?.type}
+    >
+      <div
+        className={`py-5 md:py-5 px-3 w-full flex justify-center`}
+        {...createItemSmartLink(props.item.system.id, props.item.system.name)}
       >
         <div
-          className={`py-5 md:py-5 px-3 w-full flex justify-center`}
+          className={`${mainColorTextClass[siteCodename]} m-0 text-3xl tracking-wide font-semibold text-center`}
+          {...createElementSmartLink(
+            contentTypes.hero_unit.elements.title.codename
+          )}
         >
-          <div
-            className={`${mainColorTextClass[siteCodename]} m-0 text-3xl tracking-wide font-semibold text-center`}
-            {...createElementSmartLink(
-              contentTypes.hero_unit.elements.title.codename
-            )}
-          >
-            {props.item.elements.title.value}
-          </div>
+          {props.item.elements.title.value}
         </div>
-        {bannerContent()}
-      </HeroImage>
-    </div>
+      </div>
+      {bannerContent()}
+    </HeroImage>
   );
 };
 
