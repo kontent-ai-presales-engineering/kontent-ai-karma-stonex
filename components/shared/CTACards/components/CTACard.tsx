d0f2640cb178af4ef433f6e43e237d77fe3f2523
Michal Majer <Michal.Majer@StoneX.com>
2024-04-23 23:40:40 +0200

    Improve Tailwind styles for cards

--- components/shared/CTACards/components/CTACard.tsx
+++ components/shared/CTACards/components/CTACard.tsx
@@ -13,18 +13,19 @@ export const CTACardComponent = (
   {item}: CTACardComponentProps) => {
   return <div className={"theme-card"}>
     {item.elements.image?.value?.length > 0 &&
-        <figure className={"theme-card__image"}>
+        <figure className={"w-full h-[200px] overflow-hidden"}>
             <Image
                 src={item.elements.image.value[0].url}
                 alt={item.elements.image.value[0].description || item.elements.image.value[0].name}
                 width={400}
                 height={400}
                 loading={"lazy"}
+                className={"object-cover object-center w-full h-full"}
             />
         </figure>
     }
 
-    <div className={"theme-card__content"}>
+    <div className={"flex flex-col items-start p-xxs gap-xs w-full flex-1"}>
       {!!item.elements.headingOptionsTitle?.value &&
           <Heading level={item.elements.headingOptionsHeadingLevel?.value}>
             {item.elements.headingOptionsTitle.value}
