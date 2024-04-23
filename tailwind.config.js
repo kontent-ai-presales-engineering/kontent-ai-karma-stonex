b75b9d9fe16122f7bf4999668bd28c92f8dfe81a
Michal Majer <Michal.Majer@StoneX.com>
2024-04-23 16:58:13 +0200

    First touch of Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const { filterTokensByType } = require("./tailwind/fns");
+const tokens = require("./tailwind/tokens-variables.json")
+
+const colors = filterTokensByType('color', tokens)
+const spacing = filterTokensByType('spacing', tokens)
+const borderRadius = filterTokensByType('borderRadius', tokens)
+const boxShadow = filterTokensByType('boxShadow', tokens)
+const fontSize = filterTokensByType('fontSizes', tokens)
+
 module.exports = {
   content: [
     './app/**/*.{ts,tsx}',
@@ -16,7 +26,17 @@ module.exports = {
         },
         kontentai: '#3dcca8',
         elitebuild: '#0078d2',
+        ...colors
       },
+
+      
+    ...spacing,
+    borderRadius: borderRadius.border,
+    boxShadow: boxShadow.shadow,
+    fontSize: {
+      ...fontSize.size.heading,
+      ...fontSize.size.body,
+    }
     },
   },
   plugins: [require('@tailwindcss/typography')],
